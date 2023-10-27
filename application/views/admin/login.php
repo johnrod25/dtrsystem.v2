a319f6f9420b41b2a827fbd96d68627f0f8b1d6f
Johnrod Acosta Malsi <134279774+johnrod25@users.noreply.github.com>
2023-10-27 14:58:30 +0700

    added logo

--- application/views/admin/login.php
+++ application/views/admin/login.php
@@ -27,13 +27,20 @@
         width: 350px;
 
     }
+    .logo-img img{
+        height:50px;
+    }
   </style>
 </head>
 <body>
     <div class="container d-flex justify-content-center align-items-center">
         <div class="card d-flex m-5 py-4 px-3">
         <?php echo form_open('Home/login'); ?>
-            <h1 class="text-center">DTR SYSTEM</h1>
+            <!-- <h1 class="text-center">DTR SYSTEM</h1> -->
+            <div class="col-md-12 logo-img d-flex justify-content-center align-items-center mb-3">
+            <img src="<?php echo base_url(); ?>assets/dist/img/logo.jpg" alt="RBASASHS" class="brand-image img-circle elevation-3" style="opacity: .8">
+            <h1 class="text-center ml-2">DTR SYSTEM</h1>
+            </div>
             <div class="col-md-12">
                 <div class="form-group">
                   <label>Username:</label>
