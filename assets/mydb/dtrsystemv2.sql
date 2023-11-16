4f9f47bef5c519f4ab9ead9eeb5f407f2f3f277c
Johnrod Acosta Malsi <134279774+johnrod25@users.noreply.github.com>
2023-11-16 11:29:58 +0700

    11/16/2023

--- assets/mydb/dtrsystemv2.sql
+++ assets/mydb/dtrsystemv2.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 25, 2023 at 03:44 PM
+-- Generation Time: Nov 16, 2023 at 05:27 AM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.0.28
 
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `dtrsystem`
+-- Database: `dtrsystemv2`
 --
 
 -- --------------------------------------------------------
@@ -38,16 +38,6 @@ CREATE TABLE `attendance_tbl` (
   `log_date` date NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Dumping data for table `attendance_tbl`
---
-
-INSERT INTO `attendance_tbl` (`id`, `rfid`, `fullname`, `morning_in`, `morning_out`, `afternoon_in`, `afternoon_out`, `log_date`) VALUES
-(1, 1, 'john acc malss', '05:12:24', '05:12:27', NULL, NULL, '2023-10-02'),
-(2, 5, 'juan dela cruz', '05:12:32', '05:12:34', NULL, NULL, '2023-10-02'),
-(3, 1, 'john acc malss', '05:12:58', '05:13:00', NULL, NULL, '2023-10-22'),
-(4, 5, 'juan dela cruz', '05:13:06', '05:13:08', NULL, NULL, '2023-10-22');
-
 -- --------------------------------------------------------
 
 --
@@ -66,13 +56,24 @@ CREATE TABLE `department_tbl` (
 --
 
 INSERT INTO `department_tbl` (`id`, `department_name`, `department_desc`, `date`) VALUES
-(1, 'englishhh', 'englishhh', '2023-10-07 18:07:46'),
-(2, 'mathhh', 'sdigjdlf', '2023-10-07 18:36:49'),
-(3, 'qwefrgd', 'dfgnh', '2023-10-07 18:54:34'),
-(4, 'qwefrgd', 'dfgnhsdf', '2023-10-07 18:54:41'),
-(5, 'das', 'sdcsvsd', '2023-10-17 22:04:06'),
-(6, 'cszdad', 'DSCSD', '2023-10-17 23:06:25'),
-(7, 'khb', 'HGC', '2023-10-22 04:04:26');
+(1, 'English', 'enggg', '2023-11-11 18:32:58');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `image_tbl`
+--
+
+CREATE TABLE `image_tbl` (
+  `id` int(11) NOT NULL,
+  `rfid` varchar(255) NOT NULL,
+  `fullname` varchar(255) NOT NULL,
+  `morning_in` varchar(255) NOT NULL,
+  `morning_out` varchar(255) NOT NULL,
+  `afternoon_in` varchar(255) NOT NULL,
+  `afternoon_out` varchar(255) NOT NULL,
+  `log_date` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -82,6 +83,7 @@ INSERT INTO `department_tbl` (`id`, `department_name`, `department_desc`, `date`
 
 CREATE TABLE `login_tbl` (
   `id` int(11) NOT NULL,
+  `rfid` varchar(255) NOT NULL,
   `username` varchar(100) NOT NULL,
   `password` varchar(100) NOT NULL,
   `usertype` int(11) NOT NULL,
@@ -92,9 +94,10 @@ CREATE TABLE `login_tbl` (
 -- Dumping data for table `login_tbl`
 --
 
-INSERT INTO `login_tbl` (`id`, `username`, `password`, `usertype`, `status`) VALUES
-(1, 'john', '1234', 1, 1),
-(2, 'juan', '1234', 2, 1);
+INSERT INTO `login_tbl` (`id`, `rfid`, `username`, `password`, `usertype`, `status`) VALUES
+(1, '1234567890', 'admin', 'admin', 1, 1),
+(2, '1234567899', 'skjdf.eef@g.v', 'CruzJD2222', 2, 1),
+(3, '1037077282', 'pedro.duko@g.c', 'DukoPP4222', 2, 1);
 
 -- --------------------------------------------------------
 
@@ -104,23 +107,13 @@ INSERT INTO `login_tbl` (`id`, `username`, `password`, `usertype`, `status`) VAL
 
 CREATE TABLE `schedule_tbl` (
   `id` int(11) NOT NULL,
-  `staff_id` int(11) NOT NULL,
+  `staff_id` varchar(255) NOT NULL,
   `fullname` varchar(255) NOT NULL,
   `morning_in` time DEFAULT NULL,
   `morning_out` time DEFAULT NULL,
   `afternoon_in` time DEFAULT NULL,
   `afternoon_out` time DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `schedule_tbl`
---
-
-INSERT INTO `schedule_tbl` (`id`, `staff_id`, `fullname`, `morning_in`, `morning_out`, `afternoon_in`, `afternoon_out`) VALUES
-(1, 5, 'juan dela cruz', '05:00:00', '05:52:00', '04:00:00', '04:34:00'),
-(2, 123, 'dgas asd wer', '10:00:00', '11:00:00', '16:00:00', '17:00:00'),
-(3, 7, 'pedro pen duko', '09:00:00', '11:00:00', '17:00:00', '20:00:00'),
-(4, 1, 'john acc malss', '05:00:00', '05:05:00', '17:00:00', '17:00:00');
 
 -- --------------------------------------------------------
 
@@ -130,11 +123,11 @@ INSERT INTO `schedule_tbl` (`id`, `staff_id`, `fullname`, `morning_in`, `morning
 
 CREATE TABLE `staff_tbl` (
   `id` int(11) NOT NULL,
-  `rfid` int(11) NOT NULL,
+  `rfid` varchar(255) NOT NULL,
   `firstname` varchar(255) NOT NULL,
   `midname` varchar(255) NOT NULL,
   `lastname` varchar(255) NOT NULL,
-  `department_id` int(11) NOT NULL,
+  `department_id` int(11) DEFAULT NULL,
   `dob` date DEFAULT NULL,
   `gender` varchar(255) NOT NULL,
   `civil_status` varchar(255) NOT NULL,
@@ -150,8 +143,9 @@ CREATE TABLE `staff_tbl` (
 --
 
 INSERT INTO `staff_tbl` (`id`, `rfid`, `firstname`, `midname`, `lastname`, `department_id`, `dob`, `gender`, `civil_status`, `number`, `email`, `address`, `schedule`, `log_date`) VALUES
-(1, 1, 'john', 'acc', 'malss', 3, '2001-02-04', 'Male', 'Single', '4324', 'wef.sdf#@asfd.csd', 'sdfssd', 1, '2023-10-18'),
-(2, 5, 'juan', 'delacc', 'cruz', 3, '2001-02-04', 'Male', 'Single', '4324', 'wef.sdf#@asfd.csd', 'sdfssd', 1, '2023-10-18');
+(1, '1234567890', 'admin', '', '', NULL, '2023-10-09', 'Male', 'single', '0092349023', 'admin@g.c', 'qrwerwer', NULL, '2023-11-11'),
+(2, '1234567899', 'Juan', 'Dela', 'Cruz', 1, '2222-02-22', 'Male', 'Single', '08934389279', 'skjdf.eef@g.v', 'asfsasasd', NULL, '2023-11-11'),
+(3, '1037077282', 'Pedro', 'Pen', 'Duko', 1, '4222-02-12', 'Male', 'Single', '0987654323', 'pedro.duko@g.c', 'yukjdbsvsdas dsfdf', NULL, '2023-11-12');
 
 --
 -- Indexes for dumped tables
@@ -167,6 +161,12 @@ ALTER TABLE `attendance_tbl`
 -- Indexes for table `department_tbl`
 --
 ALTER TABLE `department_tbl`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `image_tbl`
+--
+ALTER TABLE `image_tbl`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -195,31 +195,37 @@ ALTER TABLE `staff_tbl`
 -- AUTO_INCREMENT for table `attendance_tbl`
 --
 ALTER TABLE `attendance_tbl`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `department_tbl`
 --
 ALTER TABLE `department_tbl`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `image_tbl`
+--
+ALTER TABLE `image_tbl`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `login_tbl`
 --
 ALTER TABLE `login_tbl`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `schedule_tbl`
 --
 ALTER TABLE `schedule_tbl`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `staff_tbl`
 --
 ALTER TABLE `staff_tbl`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
